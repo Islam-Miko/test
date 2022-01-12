5c57044dc606f71712271feca26c58698947d44b
Islam-Miko <islamimanhod@gmail.com>
2022-01-12 18:51:25 +0600

    add new method to Person class

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ class Home:
 
 
 class Person:
+    hour_rate = 250
     def __init__(self, name, age):
         self.name = name
         self.__age = age
         self.__money = 0
         self.__house = None
+
+    def work(self, hours):
+        pay = hours * Person.hour_rate
+        self.__money += pay
+        print(f'{self.name} worked {hours} hours and earned {pay} som')
