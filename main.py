f524ac1b93ab44e3eee2a2a813e95841a2bd4dc1
Islam-Miko <islamimanhod@gmail.com>
2022-01-12 19:06:27 +0600

    write buy house method

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ class Home:
         self.name = name
         self.__price = square * Home.square_rate
 
+    @property
+    def price(self):
+        return self.__price
+
+
+house_market = [
+    Home('1 комната', 50),
+    Home('2 komnata', 65),
+    Home('3 komnata', 120)
+]
 
 class Person:
     hour_rate = 250
@@ -18,3 +28,13 @@ class Person:
         pay = hours * Person.hour_rate
         self.__money += pay
         print(f'{self.name} worked {hours} hours and earned {pay} som')
+
+    def buy_house(self):
+        for house in house_market:
+            print(f'{house.name}: {house.price}')
+            if self.__money >= house.price:
+                choice = input('Buy? Y|N').upper() == 'Y'
+                if choice:
+                    self.__house = house
+                    self.__money -= house.price
+                    house_market.remove(house)
